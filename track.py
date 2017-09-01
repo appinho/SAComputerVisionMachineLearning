Added parameter tuning and fixed covariance bug in visualization

track.py
@@ -6,12 +6,12 @@ class Track(object):
         self.number_of_states = 4 # x,y,vx,vy
         self.x = [object.x]
         self.xp = self.x
-        self.P = [np.matrix('''
-                  0.4   0.  0.  0.;
-                  0.    0.4 0.  0.;
-                  0.    0.  1.  0.;
-                  0.    0.  0.  1.
-                  ''')]
+        self.P = [np.matrix([
+            [0.3    ,    0  ,    0  ,       0],
+            [0      ,0.3    ,    0  ,       0],
+            [0      ,0      ,2      ,       0],
+            [0      ,0      ,0      ,       2]
+        ])]
         self.Pp = self.P
         self.z = [np.matrix([])]
         self.width = [object.width]

tracking_kalman_filter.py
@@ -3,11 +3,19 @@ from numpy.linalg import inv
 
 class KalmanFilter(object):
 
-    def __init__(self,number_of_states,number_of_observations):
+    def __init__(self,number_of_states,number_of_observations,var_z,var_a,dT=0.1):
         self.number_of_states = number_of_states
         self.number_of_observations = number_of_observations
-        self.Q = np.matrix(0.1*np.eye(self.number_of_states))
-        self.R = np.matrix(0.1*np.eye(self.number_of_observations))
+        #process noise covariance:
+        self.Q = var_a * np.matrix([
+            [(dT**4)/4,     (dT**4)/4,     (dT**3)/2,     (dT**3)/2],
+            [(dT**4)/4,     (dT**4)/4,     (dT**3)/2,     (dT**3)/2],
+            [(dT**3)/2,     (dT**3)/2,      dT**2,          dT**2],
+            [(dT**3)/2,     (dT**3)/2,      dT**2,          dT**2]
+        ])
+        print self.Q
+        self.R = np.matrix(var_z*np.eye(self.number_of_observations))
+        print self.R
         self.H = np.matrix(np.eye(self.number_of_observations,self.number_of_states))
         self.F = np.matrix(np.eye(self.number_of_states))
         self.I = np.matrix(np.eye(self.number_of_states))

visualization.py
@@ -99,8 +99,8 @@ def show_prediction_and_update(objects,labeling,tracks):
         plt.plot(xp,yp,'o',color=c)
 
         #calculate ellipses
-        cov = track.P[-1][np.ix_([0, 2],[0, 2])]
-        covp = track.Pp[-1][np.ix_([0, 2], [0, 2])]
+        cov = track.P[-1][:2,:2]
+        covp = track.Pp[-1][:2,:2]
         vals, vecs = eigsorted(cov)
         valsp, vecsp = eigsorted(covp)
         theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))